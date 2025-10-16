3a412acdeb5310217c54f64b87a4d141a3a06d1e
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2025-10-16 00:24:04 -0600

    Updated Soduko game

--- Sudoku/src/main/java/com/games/Sudoku.java
+++ Sudoku/src/main/java/com/games/Sudoku.java
@@ -6,10 +6,50 @@ import javax.swing.*;
 
 public class Sudoku {
 
+    String[] puzzle = {
+            "--74916-5",
+            "2---6-3-9",
+            "-----7-1-",
+            "-586----4",
+            "--3----9-",
+            "--62--187",
+            "9-4-7---2",
+            "67-83----",
+            "81--45---"
+    };
+
+    String[] solution = {
+            "387491625",
+            "241568379",
+            "569327418",
+            "758619234",
+            "123784596",
+            "496253187",
+            "934176852",
+            "675832941",
+            "812945763"
+    };
+
+    class Tile extends JButton {
+        int r;
+        int c;
+        Tile(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
     int boardwidth = 600;
     int boardHeight = 650;
 
     JFrame frame = new JFrame("Sudoku");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    JButton numSelected = null;
+    int errors = 0;
 
     Sudoku() {
         frame.setVisible(true);
@@ -18,5 +58,56 @@ public class Sudoku {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Sudoku: 0");
+
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(9,9));
+        setupTiles();
+        frame.add(boardPanel, BorderLayout.CENTER);
+
+        buttonsPanel.setLayout(new GridLayout(1, 9));
+        setupButtons();
+        frame.add(buttonsPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+
+    void setupTiles(){
+        for(int r = 0; r < 9; r++ ) {
+            for (int c = 0; c < 9; c++) {
+                Tile tile = new Tile(r,c);
+                char tileChar = puzzle[r].charAt(c);
+                tile.setText(String.valueOf(tileChar));
+                tile.setFocusable(false);
+                boardPanel.add(tile);
+            }
+        }
+    }
+
+    void setupButtons() {
+        for (int i = 1; i < 10; i++) {
+            JButton button = new JButton();
+            button.setFont(new Font("Arial", Font.BOLD, 20));
+            button.setText(String.valueOf(i));
+            button.setFocusable(false);
+            button.setBackground(Color.white);
+            buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton button = (JButton) e.getSource();
+                    if (numSelected != null) {
+                        numSelected.setBackground(Color.white);
+                    }
+                    numSelected = button;
+                    numSelected.setBackground(Color.lightGray);
+                }
+            });
+        }
     }
 }
