edaf23e8388b1dce969d662c3a941bb08f6ff9fe
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2025-12-02 18:17:06 -0700

    Added the block class to PacMan

--- PacMan/src/main/java/com/games/PacMan.java
+++ PacMan/src/main/java/com/games/PacMan.java
@@ -8,7 +8,33 @@ import javax.swing.*;
 
 public class PacMan extends JPanel {
 
-    private int rowCount = 21;
+
+    class Block {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+
+        int startX;
+        int startY;
+        char direction = 'U'; // U D L R
+        int velocityX = 0;
+        int velocityY = 0;
+
+        Block(Image image, int x, int y, int width, int height) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
+
+
+        private int rowCount = 21;
     private int columnCount = 19;
     private int tileSize = 32;
     private int boardWidth = columnCount * tileSize;
